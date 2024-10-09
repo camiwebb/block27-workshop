e2c4d0e64f3792d99ae4ba08314be10077d6b3b4
Camille Webb <camillewebb35@gmail.com>
2024-10-08 19:57:09 -0600

    completed the sign up component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
 import './App.css'
+import Authenticate from "./Components/Authenticate";
+import SignUpForm from "./Components/SignUpForm";
 
 function App() {
-  const [] = useState()
+  // const [] = useState("")
 
   return (
     <>
-      
+      <Authenticate />
+      <SignUpForm />
     </>
-  )
+  );
 }
 
 export default App
