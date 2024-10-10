ad5df26f8f8af460b6419344f72af8866d1a4c1e
Camille Webb <camillewebb35@gmail.com>
2024-10-10 16:44:38 -0600

    working on the authenticate functionality

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-function SignUpForm() {
+function SignUpForm({ setToken }) {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [error, setError] = useState(null);
@@ -15,6 +15,10 @@ function SignUpForm() {
            });
            const result = await response.json();
            console.log(result);
+
+           if (result.token) {
+            setToken(result.token);
+           }
         } catch (error) {
             setError(error.message);
         }
